be4c131d84b5ca261d50392be195a073c2923617
varinimittal <47278598+varinimittal@users.noreply.github.com>
2020-12-22 22:46:53 +0530

    Update Casescomparator.java

--- coronavirus/src/main/java/com/comparingCases/Casescomparator.java
+++ coronavirus/src/main/java/com/comparingCases/Casescomparator.java
@@ -8,7 +8,7 @@ public class Casescomparator implements Comparator<LocationStatistics>
 {
 	public int compare(LocationStatistics  ar0,LocationStatistics  ar1)
 	{
-		return ar0.latestTotalCases-ar1.latestTotalCases;
+		return ar1.latestTotalCases-ar0.latestTotalCases;
 	}
 	
 }
